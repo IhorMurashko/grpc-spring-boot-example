a83ecd2b038b2462756ee8ff5d8c472894f2a44f
Ihor Murashko <i.murashko0911@gmail.com>
2024-10-09 11:04:36 +0200

    communicating client-server

---
+++ grpc-client-demo/src/test/java/com/test/multimodule/grpcclientdemo/GrpcClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.multimodule.grpcclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
